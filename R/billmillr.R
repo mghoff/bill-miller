7c892d68c2f4743259ca89ec31869de2d1e5754b
Matthew Hoff <matthew.g.hoff@gmail.com>
2022-03-13 08:39:06 -0400

    addback 'saved' data frame input, but make it hidden with a .input as it is not truly a user input, but necessary for the initial step in recursion.

--- R/billmillr.R
+++ R/billmillr.R
@@ -6,13 +6,16 @@
 #' @param numCoins integer, total number of coin flips
 #' @param minHeads integer, minimum number of heads to to obtained in a single streak
 #' @param probHeads number, probability of obtaining a heads on a single coin toss
+#' @param .saved data frame, contains previously computed probabilities for a given index;
+#' used to speed up recursion so not to recalculate every probability should an already seen
+#' scenario of numCoins, minHeads, and probHeads arises.
 #'
 #' @return numeric, odds of streak
 #' @export
 #'
 #' @examples
 #' oddsOfStreak(numCoins = 10, minHeads = 3, probHeads = 0.5)
-oddsOfStreak <- function(numCoins = 10, minHeads = 5, probHeads = 0.5) {
+oddsOfStreak <- function(numCoins = 10, minHeads = 5, probHeads = 0.5, .saved = NULL) {
     # Computes the probability (i.e. S[n, k]) of getting a run of minHeads
     # i.e. the odds of getting K or more heads in a row out of
     # N independent coin tosses where the p is the probability of Heads
@@ -20,19 +23,18 @@ oddsOfStreak <- function(numCoins = 10, minHeads = 5, probHeads = 0.5) {
 
     # Using recursion: S[N, K] = p^K + sum_{j=1, K} ( p^(j-1) * (1-p) * S[N-j, K] )
     # As well as base cases: 1.) S[0, K] = 0 and 2.) S[N, K] = 0
-    saved <- NULL
-    if (is.null(saved)) {
-      saved = data.frame(ID = character(),
-                         result = numeric(),
-                         stringsAsFactors = F)
+    if (is.null(.saved)) {
+      .saved = data.frame(ID = character(),
+                          result = numeric(),
+                          stringsAsFactors = F)
     }
 
     # Get a unique identifier for computed value
     ID <- paste0(numCoins, minHeads, probHeads)
 
     # If it has been computed before, return pre-computed value
-    if (ID %in% saved$ID) {
-        return(as.numeric(saved[which(saved$ID == ID), 2]))
+    if (ID %in% .saved$ID) {
+        return(as.numeric(.saved[which(.saved$ID == ID), 2]))
     # Handle cases where we have no coins, or more heads than coins
     } else if (minHeads > numCoins | numCoins <= 0) {
         result <- 0
@@ -42,11 +44,11 @@ oddsOfStreak <- function(numCoins = 10, minHeads = 5, probHeads = 0.5) {
       result <- probHeads**minHeads # S[n, k] = p^k + ...
       # S[n, k] = ... + sum_{j=1, k} p^(j-1) * (1-p) * S[n-j, k]
       for (firstTail in seq(1, minHeads+1)) {
-        pr <- oddsOfStreak(numCoins-firstTail, minHeads, probHeads)
+        pr <- oddsOfStreak(numCoins-firstTail, minHeads, probHeads, .saved)
         result <- result + (probHeads**(firstTail-1))*(1-probHeads)*pr
       }
       # Save computed value for later use, if needed; i.e. if prob has already been calced
-      saved <- data.frame(rbind(saved, cbind(ID, result)), stringsAsFactors = F)
+      .saved <- data.frame(rbind(.saved, cbind(ID, result)), stringsAsFactors = F)
     }
   # Return computed value
   return(result)
