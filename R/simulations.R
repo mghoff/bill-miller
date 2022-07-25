f713c34b252a01644cbdcb5ab2558e3a592011d8
Matthew Hoff <matthew.g.hoff@gmail.com>
2022-07-25 15:24:27 -0400

    refactor simulation functions for non-numeric and non-binomial sample spaces

--- R/simulations.R
+++ R/simulations.R
@@ -1,19 +1,28 @@
-#' Find the Longest Run
+#' Calculate the Longest Run
 #'
-#' Finds the single longest run (streak) of the value "1"
-#' provided from a binomial sample of size n with probability
+#' Finds the single longest run (streak) of a specified value
+#' contained within a provided vector
 #'
-#' @param binom_sample numeric vector, a binomial sample; e.g. \code{sample(c(0, 1), size = 5, replace = TRUE)}
+#' @param sample numeric or character vector.
+#' @param run_value number or character, the value on which to count sequential occurrences.
 #'
-#' @return number, representing the longest streak (run of "1") in the provided sample
+#' @return number, the longest run (streak) of values
 #' @export
 #'
 #' @examples
-#' find_longest_run(binom_sample = sample(c(0, 1), size = 25, replace = TRUE))
-find_longest_run <- function(binom_sample) {
+#' s <- sample(c(0, 1), size = 10, replace = TRUE)
+#' print(s)
+#'
+#' find_longest_run(sample = s, run_value = 1)
+find_longest_run <- function(sample, run_value = 1) {
+  # Arg Check
+  sample_vals <- unique(sample)
+  if (!(run_value %in% sample_vals)) stop("run_value not found in sample")
+
+  # Count
   run <- max_run <- 0
-  for (i in 2:length(binom_sample)) {
-    if (binom_sample[i] == 1 && binom_sample[i] == binom_sample[i - 1]) {
+  for (i in 2:length(sample)) {
+    if (sample[i] == run_value && sample[i] == sample[i - 1]) {
       run <- run + 1
       max_run <- max(max_run, run)
     } else {
@@ -30,21 +39,28 @@ find_longest_run <- function(binom_sample) {
 #' Out of a given number of trials flipping a fair coin (p = 0.5) n times, count the number of
 #' times a run (streak) of length k occurs
 #'
-#' @param trials number, number of attempts to flip a coin n times
-#' @param flips number, number of sequential coin flips in which a streak could be counted
-#' @param streak_length number, length of streak of interest
+#' @param trials number, number of attempts to flip a coin n times.
+#' @param sample_space vector, vector of unique values from which to sample.
+#' @param sample_size number, size of sample to generate from \code{sample_space}.
+#' @param run_value number or character, the value on which to count sequential occurrences.
+#' @param run_length number, length of streak of interest.
 #'
 #' @return number
 #' @export
 #'
 #' @examples
 #' count_runs()
-count_runs <- function(trials = 1000, flips = 40, streak_length = 15) {
+count_runs <- function(trials = 1000,
+                       sample_space = c(0, 1),
+                       sample_size = 40,
+                       run_value = 1,
+                       run_length = 15) {
   x <- NULL
   total_applicable_runs <- 0
   for (t in 1:trials) {
-    x[t] <- find_longest_run(binom_sample = sample(c(0, 1), size = flips, replace = TRUE))
-    if (x[t] >= streak_length) {
+    x[t] <- find_longest_run(sample = sample(sample_space, size = sample_size, replace = TRUE),
+                             run_value = run_value)
+    if (x[t] >= run_length) {
       total_applicable_runs <- total_applicable_runs + 1
     }
   }
@@ -64,21 +80,28 @@ count_runs <- function(trials = 1000, flips = 40, streak_length = 15) {
 #' 2.) the count of applicable trials in that iteration, and
 #' 3.) the cumulative probability of obtaining zero streaks.
 #'
-#' @param iters number, number of simulation iterations to consider
+#' @param iters number, number of simulation iterations
 #' @param trials number, see \code{count_runs()}
-#' @param flips number, see \code{count_runs()}
-#' @param streak_length number, see \code{count_runs()}
+#' @param sample_space vector, see \code{count_runs()}
+#' @param sample_size number, see \code{count_runs()}
+#' @param run_value number, see \code{count_runs()}
+#' @param run_length number, see \code{count_runs()}
 #'
-#' @return data frame
+#' @return data frame with 3 columns; the number of rows determined by \code{iters}
 #' @export
 #'
 #' @examples
 #' tail(run_simulation())
-run_simulation <- function(iters = 100, trials = 1000, flips = 40, streak_length = 15) {
+run_simulation <- function(iters = 100,
+                           trials = 1000,
+                           sample_space = c(0, 1),
+                           sample_size = 40,
+                           run_value = 1,
+                           run_length = 15) {
   d <- data.frame(stringsAsFactors = FALSE)
   for (i in 1:iters) {
     d[i, 1] <- i
-    d[i, 2] <- count_runs(trials = trials, flips = flips, streak_length = streak_length)
+    d[i, 2] <- count_runs(trials, sample_space, sample_size, run_value, run_length)
     d[i, 3] <- round(nrow(d[which(d[, 2] == 0), ]) / nrow(d), 6)
   }
   names(d) <- c("iteration", "applicable_trials", "prob_of_zero")
